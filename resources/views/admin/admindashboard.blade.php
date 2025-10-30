c9687cf3c32990fd293d46b57b64e3d7ea71bba1
Nick Dagum <147642158+panckeyk@users.noreply.github.com>
2025-10-31 05:20:58 +0800

    added search and group button in nav

---
+++ resources/views/admin/admindashboard.blade.php
@@ -0,0 +1,3 @@
+<x-navbar :is-admin-dashboard="true">
+    <h1> Hello World </h1>
+</x-navbar>
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,13 @@ Route::get('/', function () {
 Route::get('/user', function () {
     return view('user.userlayoutpage');
 })->name('user.page');
+
+Route::get('/admin', function () {
+    return view('admin.adminlayoutpage');
+})->name('admin.page');
+
+
+Route::get('/admindashboard', function () {
+    return view('admin.admindashboard');
+})->name('admin.dashboard');
+
