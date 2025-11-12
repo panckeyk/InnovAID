252e5b7164c62efede9117d7e68edce3c7be8c71
jellablue <143723671+jellablue@users.noreply.github.com>
2025-11-12 17:25:08 +0800

    trust proxies

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request as HttpRequest;
+
+class TrustProxies
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array<int, string>|string|null
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
+
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->setTrustedProxies($this->proxies === '*' ? ['*'] : (array) $this->proxies, $this->headers);
+
+        return $next($request);
+    }
+}
